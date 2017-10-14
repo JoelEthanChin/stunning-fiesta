f43444ae26887904ecf9b3595b929229bb11b5f6
Joel Chin <jechin2@hotmail.com>
2017-10-13 21:19:25 -0400

    I have 3 textboxes for date, time and weather, but the size of weather isn't correct

--- basicgui.py
+++ basicgui.py
@@ -1,12 +1,27 @@
 from Tkinter import *
 
+
 window = Tk()
 window.geometry("1280x720")
 window.title("Smart Clock")
+#textbox for the date information
+date = Text(window, bg="blue", width=90)
+date.insert(INSERT, "\n" * 11 + "Date Info")
+date.tag_configure("center", justify='center')
+date.tag_add("center", 0.0, "end")
+date.grid(row=0, column=0)
+#textbox for the time information
+time = Text(window, bg="red", width=90)
+time.insert(INSERT, "\n" * 11 + "Time Info")
+time.tag_configure("center", justify='center')
+time.tag_add("center", 0.0, "end")
+time.grid(row=720, column=0)
+#texbox for the weather information
+weath = Text(window, bg="yellow", width=90)
+weath.insert(INSERT, "\n" * 11 + "Weather Info")
+weath.tag_configure("center", justify='center')
+weath.tag_add("center", 0.0, "end")
+weath.grid(row=0, column=1280)
 
-t = Text(window)
-t.insert(INSERT, "Hello")
-t.insert(END, "Bye Bye.....")
-t.pack(side = LEFT)
 
 window.mainloop()
